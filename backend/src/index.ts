a9242fb503a50ef7a4035f84bd11802fe5e4e4c8
Nirjaraa <nirjarathapa11@gmail.com>
2025-03-31 12:00:00 +0545

    server code updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,17 +10,39 @@ const io = new Server(server, {
   },
 });
 
+const rooms: Record<string, Socket[]> = {};
+
 io.on("connection", (socket: Socket) => {
   console.log("A user connected:", socket.id);
 
-  socket.on("message", (msg) => {
-    console.log("Message received from client:", msg);
-    socket.emit("message", "Hi from the server!");
+  socket.on("joinroom", (roomId: string) => {
+    console.log(`${socket.id} is in the ${roomId}`);
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    if (rooms[roomId].length >= 2) {
+      socket.emit("Room is filled");
+    }
+
+    rooms[roomId].push(socket);
+    socket.join(roomId);
+    const playerColor = rooms[roomId].length === 1 ? "white" : "black";
+    socket.emit("player-color", playerColor);
+    if (rooms[roomId].length === 2) {
+      io.to(roomId).emit("start the game");
+    }
   });
-  socket.emit("message", "Hi from the server!");
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter((s) => s.id !== socket.id);
+      if (rooms[roomId].length === 1)
+        socket.emit("Other player disconnected.Game-over");
+      if (rooms[roomId].length === 0) delete rooms[roomId];
+    }
   });
 });
 
